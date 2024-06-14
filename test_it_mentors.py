6504265f7fee5394111e6f1e5ac2cf57a5093b34
Alexandr Kirichenko <sakoz@mail.ru>
2024-06-14 17:54:17 +0500

    2 change

--- test_it_mentors.py
+++ test_it_mentors.py
@@ -2,13 +2,13 @@ expression = input("В ведите пример: ")
 try:
     parts = expression.split(' ')
     if len(parts) != 3:
-        raise ValueError("throws Exception: Ввод должен содержать два числа и один оператор, разделенные пробелами")
+        raise ValueError("throws Exception: Ввод должен содержать два числа и один оператор (+, -, /, *), разделенные пробелами")
 
     a = int(parts[0])
     operator = parts[1]
     b = int(parts[2])
 
-    if not (0 <= a <= 10) or not (0 <= b <= 10):
+    if not (1 <= a <= 10) or not (1 <= b <= 10):
         raise ValueError("throws Exception: Числа должны быть в диапазоне от 1 до 10 включительно")
     if operator == '+':
         result = a + b
@@ -17,8 +17,6 @@ try:
     elif operator == '*':
         result = a * b
     elif operator == '/':
-        if b == 0:
-            raise ZeroDivisionError("Деление на ноль невозможно")
         result = a // b
     else:
         raise ValueError("throws Exception: Некорректный оператор. Допустимые операторы: +, -, *, /")
@@ -27,5 +25,3 @@ try:
 
 except ValueError as ve:
     print(f"Ошибка: {ve}")
-except ZeroDivisionError as zde:
-    print(f"Ошибка: {zde}")
