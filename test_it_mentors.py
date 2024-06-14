d0e1ff50309ef879c3e7c1734c83e0611116ddd7
Alexandr Kirichenko <sakozdiversant@gmail.com>
2024-06-14 23:11:22 +0500

    Change 3

--- test_it_mentors.py
+++ test_it_mentors.py
@@ -1,27 +1,31 @@
-expression = input("В ведите пример: ")
-try:
-    parts = expression.split(' ')
-    if len(parts) != 3:
-        raise ValueError("throws Exception: Ввод должен содержать два числа и один оператор (+, -, /, *), разделенные пробелами")
+def main(expression):
+    try:
+        parts = expression.split(' ')
+        if len(parts) != 3:
+            raise ValueError("throws Exception: Ввод должен содержать два числа и один оператор (+, -, /, *), разделенные пробелами")
 
-    a = int(parts[0])
-    operator = parts[1]
-    b = int(parts[2])
+        a = int(parts[0])
+        operator = parts[1]
+        b = int(parts[2])
 
-    if not (1 <= a <= 10) or not (1 <= b <= 10):
-        raise ValueError("throws Exception: Числа должны быть в диапазоне от 1 до 10 включительно")
-    if operator == '+':
-        result = a + b
-    elif operator == '-':
-        result = a - b
-    elif operator == '*':
-        result = a * b
-    elif operator == '/':
-        result = a // b
-    else:
-        raise ValueError("throws Exception: Некорректный оператор. Допустимые операторы: +, -, *, /")
+        if not (1 <= a <= 10) or not (1 <= b <= 10):
+            raise ValueError("throws Exception: Числа должны быть в диапазоне от 1 до 10 включительно")
+        if operator == '+':
+            result = a + b
+        elif operator == '-':
+            result = a - b
+        elif operator == '*':
+            result = a * b
+        elif operator == '/':
+            result = a // b
+        else:
+            raise ValueError("throws Exception: Некорректный оператор. Допустимые операторы: +, -, *, /")
 
-    print(result)
+        return result
 
-except ValueError as ve:
-    print(f"Ошибка: {ve}")
+    except ValueError as ve:
+        print(f"Ошибка: {ve}")
+
+if __name__ == "__main__":
+    expression = input('Введит пример: ')
+    print(main(expression))
