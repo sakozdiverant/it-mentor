79d43e5b6f19b9c9351ca8a5726a250b6130ae25
Alexandr Kirichenko <sakozdiversant@gmail.com>
2024-07-11 00:11:51 +0500

    pz6 11задание

---
+++ pz6/pz6_part5.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Реализуйте класс Calculator, в котором будет один метод, для вычисления суммы двух чисел.
+# Реализуйте еще один класс, который будет наследоваться от класса Calculator и перегрузите метод
+# для вычисления суммы двух чисел, чтобы он делал конкатенацию двух строк.
+
+class Calculator:
+    def add(self, a, b):
+        result = a + b
+        return result
+
+class StringCalculator(Calculator):
+    def addstr(self, a, b):
+        if isinstance(a, str) and isinstance(b, str):
+            return self.add(a, b)
+        else:
+            raise TypeError("Оба аргумента должны быть строками для конкатенации")
+
+calc = Calculator()
+print(calc.add(3, 5))
+
+str_calc = StringCalculator()
+print(str_calc.addstr("Hello, ", "world!"))
+
+try:
+    print(str_calc.addstr(3, 5))
+except TypeError as e:
+    print(e)
---
+++ pz6/pz6_part6.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Реализуйте абстрактный класс Animals, создайте класс Cat,
+# который будет наследоваться от класса Animals и реализуйте метод voice.
+
+from abc import ABC, abstractmethod
+
+class Animals(ABC):
+    @abstractmethod
+    def voice(self):
+        pass
+
+class Cat(Animals):
+    def voice(self):
+        return "Meow"
+
+
+cat = Cat()
+print(cat.voice())
