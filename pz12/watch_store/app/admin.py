1163744f8e6f701494393a203df534b41dfd7f03
Alexandr Kirichenko <sakoz@mail.ru>
2024-09-24 14:46:14 +0500

    user change

--- pz12/watch_store/app/admin.py
+++ pz12/watch_store/app/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Product, Cart, CartItem
+from .models import Product, Cart, CartItem, Profile
+from django.contrib.auth.admin import UserAdmin
+
 
 admin.site.register(Product)
 admin.site.register(Cart)
 admin.site.register(CartItem)
+admin.site.register(Profile)
--- pz12/watch_store/app/models.py
+++ pz12/watch_store/app/models.py
@@ -1,6 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser
 from .product import Product
+from django.db.models.signals import post_save
+from django.core.validators import MinLengthValidator
+from django.dispatch import receiver
 
 class Cart(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -16,3 +19,22 @@ class CartItem(models.Model):
 
     def __str__(self):
         return f"{self.quantity} of {self.product.name}"
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)  # Связь с моделью User
+    phone_number = models.CharField(max_length=15, blank=True, null=True)  # Номер телефона
+    inn = models.CharField(max_length=12, blank=True, null=True, validators=[MinLengthValidator(12)])
+    def __str__(self):
+        return f"{self.user.username}'s profile"
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
+
